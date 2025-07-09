965eb0398e15354808ba76768d4760c3857e856a
Caio Bezerra Londres Barreto <caiobarreto_28@hotmail.com>
2025-07-09 12:15:43 -0300

    chore: create recover and reset password routes

---
+++ apps/api/src/http/routes/auth/request-passowrd-recover.ts
@@ -0,0 +1,47 @@
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod/v4'
+
+import { prisma } from '@/lib/prisma'
+
+export async function requestPasswordRecover(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    '/password/recover',
+    {
+      schema: {
+        tags: ['auth'],
+        summary: 'Request password recover',
+        body: z.object({
+          email: z.email(),
+        }),
+        response: {
+          201: z.null(),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { email } = request.body
+
+      const userFromEmail = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      if (!userFromEmail) {
+        return reply.status(201)
+      }
+
+      const { id: code } = await prisma.token.create({
+        data: {
+          type: 'PASSWORD_RECOVER',
+          userId: userFromEmail.id,
+        },
+      })
+
+      console.log('token:', code)
+
+      return reply.status(201).send()
+    },
+  )
+}
