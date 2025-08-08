902a47c6872b96859a1011f5b9cf153065b43f85
Caio Bezerra Londres Barreto <caiobarreto_28@hotmail.com>
2025-08-08 14:06:24 -0300

    chore: create get memebership route

---
+++ apps/api/src/http/routes/organizations/get-memebership.ts
@@ -0,0 +1,46 @@
+import { roleSchema } from '@saas/auth'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod/v4'
+
+import { auth } from '@/http/middleware/auth'
+
+export async function getMembership(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organizations/:slug/membership',
+      {
+        schema: {
+          tags: ['organizations'],
+          summary: 'Get user membership on organization',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+          }),
+          response: {
+            200: {
+              membership: z.object({
+                id: z.uuid(),
+                organizationId: z.uuid(),
+                role: roleSchema,
+              }),
+            },
+          },
+        },
+      },
+      async (request) => {
+        const { slug } = request.params
+        const { membership } = await request.getUserMembership(slug)
+
+        return {
+          membership: {
+            id: membership.id,
+            role: membership.role,
+            organizationId: membership.organizationId,
+          },
+        }
+      },
+    )
+}
