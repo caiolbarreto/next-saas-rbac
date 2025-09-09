a6835d0b56f2ddfc5bcb6a6d8699d7a23198537a
Caio Bezerra Londres Barreto <cbarreto@justworks.com>
2025-09-08 21:46:27 -0300

    chore: get invites

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -18,6 +18,7 @@ import { requestPasswordRecover } from './routes/auth/request-passowrd-recover'
 import { resetPassword } from './routes/auth/reset-password'
 import { createInvite } from './routes/invites/create-invite'
 import { getInvite } from './routes/invites/get-invite'
+import { getInvites } from './routes/invites/get-invites'
 import { getMembers } from './routes/members/get-members'
 import { updateMember } from './routes/members/update-member'
 import { createOrganization } from './routes/organizations/create-organization'
@@ -102,6 +103,7 @@ app.register(updateMember)
 // Invites routes
 app.register(createInvite)
 app.register(getInvite)
+app.register(getInvites)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running')
